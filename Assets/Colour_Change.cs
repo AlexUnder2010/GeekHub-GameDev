aa00083bb7d90ad7c2fa0bb39ea042b4c4f616c5
Alexandr Mikhailik <alex-under-2010@mail.ru>
2016-11-02 03:09:52 +0200

    Second Hometask done

---
+++ Assets/Colour_Change.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class Colour_Change : MonoBehaviour
+{
+    public GameObject Door;
+    public float Distance;
+    public float tmp;
+    // Use this for initialization
+    void Start ()
+    {
+        Door = (GameObject)gameObject; 
+    }
+	
+	// Update is called once per frame
+    void Update()
+    {
+        GameObject Player = GameObject.Find("Player").gameObject;
+        Distance = (float)(Door.transform.position - Player.transform.position).magnitude/10;
+        tmp = 1/(Mathf.Exp(Distance)-1);
+        gameObject.GetComponent<Renderer>().material.color = new Color(tmp,tmp,tmp);
+    }
+}
---
+++ Assets/Move.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Move : MonoBehaviour {
+    public GameObject player;
+    public int speedRotation = 3;
+    public int speed = 5;
+    public int jumpSpeed = 1;
+
+    void Start()
+    {
+        player = (GameObject)this.gameObject;
+        
+    }
+    void Update()
+    {
+
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
+        {
+            player.transform.position += player.transform.forward * speed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
+        {
+            player.transform.position -= player.transform.forward * speed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.UpArrow))
+        {
+            player.transform.position += player.transform.right * speed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.UpArrow))
+        {
+            player.transform.position -= player.transform.right * speed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.Q) || Input.GetKey(KeyCode.LeftArrow))
+        {
+            player.transform.Rotate(Vector3.down * speedRotation);
+        }
+        if (Input.GetKey(KeyCode.E) || Input.GetKey(KeyCode.RightArrow))
+        {
+            player.transform.Rotate(Vector3.up * speedRotation);
+        }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            player.transform.position += player.transform.up * jumpSpeed * Time.deltaTime;
+        }
+        
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        SceneManager.LoadScene(1);
+    }
+}
